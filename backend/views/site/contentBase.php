b1acab4729a0450e6ea356bfe0f4e848d102e066
Devanshu <Devanshu.mcs19.du@gmail.com>
2021-09-08 13:03:44 +0530

    content page changed

--- backend/views/site/contentBase.php
+++ backend/views/site/contentBase.php
@@ -6,16 +6,164 @@ use yii\helpers\Html;
 $this->params['breadcrumbs'] = [['label' => $this->title]];
 ?>
 
-<!-- 	<div class="row">
-		<div class="col-lg-6">
-		?= widgets\Callout::widget([
-			'type' => 'info',
-			'head' => 'This is to inform!',
-			'body' => 'Now we are on Base\'s Content page.'
-			]) ?>
-		</div>  
-	</div> -->
+
 <div class="container-fluid">
+	<div class="row align-items-center">
+		<div class="col-lg-5 col-md-6 col-sm-6 col-xs-4 col-10">
+			<div class="small-box bg-purple">
+				<div class="inner">
+					<h3>Website Header </h3>
+					<p>change the navigation bar </p>
+				</div>
+				<div class="icon">
+					<i class="fas fa-bars"></i>
+				</div>
+				<a href="#" class="small-box-footer">
+					<i class="fas fa-arrow-circle-right"></i>
+				</a>
+			</div>
+		</div>
+		<div class="col-lg-5 col-md-6 col-sm-6 col-xs-4 col-10">
+			<div class="small-box bg-light">
+				<div class="inner">
+					<h3>Website Footer</h3>
+					<p>change the footer content </p>
+				</div>
+				<div class="icon">
+					<i class="fas fa-bars"></i>
+				</div>
+				<a href="<?php echo Yii::$app->urlManager->createUrl("footer/index");?>" class="small-box-footer">
+					<i class="fas fa-arrow-circle-right"></i>
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="row align-items-center">
+		<div class="col-lg-5 col-md-6 col-sm-6 col-xs-4 col-10">
+			<div class="small-box bg-purple">
+				<div class="inner">
+					<h3>Homepage Slider</h3>
+					<p>add, remove or change a slide </p>
+				</div>
+				<div class="icon">
+					<i class="fas fa-ellipsis-h"></i>
+				</div>
+				<a href="<?php echo Yii::$app->urlManager->createUrl("home-slider/index");?>" class="small-box-footer">
+					<i class="fas fa-arrow-circle-right"></i>
+				</a>
+			</div>
+		</div>
+		<div class="col-lg-5 col-md-6 col-sm-6 col-xs-4 col-10">
+			<div class="small-box bg-light">
+				<div class="inner">
+					<h3>Recent Updates</h3>
+					<p>add, remove an update </p>
+				</div>
+				<div class="icon">
+					<i class="fas fa-edit"></i>
+				</div>
+				<a href="<?php echo Yii::$app->urlManager->createUrl("updates-panel/index");?>" class="small-box-footer">
+					<i class="fas fa-arrow-circle-right"></i>
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="row align-items-center">
+		<div class="col-lg-5 col-md-6 col-sm-6 col-xs-4 col-10">
+			<div class="small-box bg-purple">
+				<div class="inner">
+					<h3>Governing Council</h3>
+					<p>add, remove or update council member </p>
+				</div>
+				<div class="icon">
+					<i class="fab fa-mendeley"></i>
+				</div>
+				<a href="<?php echo Yii::$app->urlManager->createUrl("gov-council/index");?>" class="small-box-footer">
+					<i class="fas fa-arrow-circle-right"></i>
+				</a>
+			</div>
+		</div>
+		<div class="col-lg-5 col-md-6 col-sm-6 col-xs-4 col-10">
+			<div class="small-box bg-light">
+				<div class="inner">
+					<h3>Contact Form </h3>
+					<p>view the students messages </p>
+				</div>
+				<div class="icon">
+					<i class="fab fa-wpforms"></i>
+				</div>
+				<a href="<?php echo Yii::$app->urlManager->createUrl("contact-form/index");?>" class="small-box-footer">
+					<i class="fas fa-arrow-circle-right"></i>
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="row align-items-center">
+		<div class="col-lg-5 col-md-6 col-sm-6 col-xs-4 col-10">
+			<div class="small-box bg-purple">
+				<div class="inner">
+					<h3>News & Events</h3>
+					<p>add, remove or update news & events </p>
+				</div>
+				<div class="icon">
+					<i class="fas fa-newspaper"></i>
+				</div>
+				<a href="<?php echo Yii::$app->urlManager->createUrl("news-events/index");?>" class="small-box-footer">
+					<i class="fas fa-arrow-circle-right"></i>
+				</a>
+			</div>
+		</div>
+		<div class="col-lg-5 col-md-6 col-sm-6 col-xs-4 col-10">
+			<div class="small-box bg-light">
+				<div class="inner">
+					<h3>Leadership </h3>
+					<p>add, remove a leader </p>
+				</div>
+				<div class="icon">
+					<i class="fas fa-user-ninja"></i>
+				</div>
+				<a href="<?php echo Yii::$app->urlManager->createUrl("leadership/index");?>" class="small-box-footer">
+					<i class="fas fa-arrow-circle-right"></i>
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class="row align-items-center">
+		<div class="col-lg-5 col-md-6 col-sm-6 col-xs-4 col-10">
+			<div class="small-box bg-purple">
+				<div class="inner">
+					<h3>Careers</h3>
+					<p>add, remove or update career sections </p>
+				</div>
+				<div class="icon">
+					<i class="fas fa-newspaper"></i>
+				</div>
+				<a href="<?php echo Yii::$app->urlManager->createUrl("news-events/career");?>" class="small-box-footer">
+					<i class="fas fa-arrow-circle-right"></i>
+				</a>
+			</div>
+		</div>
+		<!-- <div class="col-lg-5 col-md-6 col-sm-6 col-xs-4 col-10">
+			<div class="small-box bg-light">
+				<div class="inner">
+					<h3>Leadership </h3>
+					<p>add, remove a leader </p>
+				</div>
+				<div class="icon">
+					<i class="fas fa-user-ninja"></i>
+				</div>
+				<a href="<?php echo Yii::$app->urlManager->createUrl("leadership/index");?>" class="small-box-footer">
+					<i class="fas fa-arrow-circle-right"></i>
+				</a>
+			</div>
+		</div> -->
+	</div>
+</div>
+
+
+
+
+<div class="container-fluid" style="display:none">
 	<div class="row justify-content-around align-items-center">
 		<div class="col-lg-3 col-sm-2 col-10">
 			<a href="<?php echo Yii::$app->urlManager->createUrl("home-slider/index");?>" class="btn btn-outline-info"><?= Html::img('@web/img/sliderbase.png',['alt' => 'Nothing to show', 'class' => 'img-fluid']);?><h2 style="font-size: 150%;">Home Slider</h2></a> 
--- backend/views/site/contentSchools.php
+++ backend/views/site/contentSchools.php
@@ -7,29 +7,102 @@ $this->params['breadcrumbs'] = [['label' => $this->title]];
 ?>
 
 <div class="container-fluid">
-	<div class="row justify-content-around align-items-center">
-		<div class="col-lg-3 col-sm-2 col-10">
-			<a href="<?php echo Yii::$app->urlManager->createUrl("/schools/schools");?>" class="btn btn-outline-info"><?= Html::img('@web/img/indexschool.png',['alt' => 'Nothing to show', 'class' => 'img-fluid']);?><h2 style="font-size: 150%;">School's Names</h2></a> 
+	<div class="row align-items-center">
+		<div class="col-lg-5 col-md-6 col-sm-6 col-xs-4 col-10">
+			<div class="small-box bg-purple">
+				<div class="inner">
+					<h3>Schools Index</h3>
+					<p>change the basic information or slider of a school</p>
+				</div>
+				<div class="icon">
+					<i class="fas fa-bars"></i>
+				</div>
+				<a href="<?php echo Yii::$app->urlManager->createUrl("/schools/schools");?>" class="small-box-footer">
+					<i class="fas fa-arrow-circle-right"></i>
+				</a>
+			</div>
 		</div>
-		<div class="col-lg-3 col-sm-2 col-10">
-			<a href="<?php echo Yii::$app->urlManager->createUrl("/schools/school-home");?>" class="btn btn-outline-info"><?= Html::img('@web/img/indexschool.png',['alt' => 'Nothing to show', 'class' => 'img-fluid']);?><h2 style="font-size: 150%;">School's Home</h2></a> 
-		</div>
-		<div class="col-lg-3 col-sm-2 col-10">
-			<a href="<?php echo Yii::$app->urlManager->createUrl("/schools/school-obj");?>" class="btn btn-outline-info"><?= Html::img('@web/img/indexschool.png',['alt' => 'Nothing to show', 'class' => 'img-fluid']);?><h2 style="font-size: 150%;">School's Objective & Vision</h2></a> 
+		<div class="col-lg-5 col-md-6 col-sm-6 col-xs-4 col-10">
+			<div class="small-box bg-light">
+				<div class="inner">
+					<h3>Home Page</h3>
+					<p>change the home page content </p>
+				</div>
+				<div class="icon">
+					<i class="fas fa-bars"></i>
+				</div>
+				<a href="<?php echo Yii::$app->urlManager->createUrl("/schools/school-home");?>" class="small-box-footer">
+					<i class="fas fa-arrow-circle-right"></i>
+				</a>
+			</div>
 		</div>
 	</div>
-	<div class="row justify-content-around align-items-center">
-		<div class="col-lg-3 col-sm-2 col-10">
-			<a href="<?php echo Yii::$app->urlManager->createUrl("/schools/school-gov-council");?>" class="btn btn-outline-info"><?= Html::img('@web/img/indexschool.png',['alt' => 'Nothing to show', 'class' => 'img-fluid']);?><h2 style="font-size: 150%;">School's Governanace Body</h2></a> 
+	<div class="row align-items-center">
+		<div class="col-lg-5 col-md-6 col-sm-6 col-xs-4 col-10">
+			<div class="small-box bg-purple">
+				<div class="inner">
+					<h3>Objective & Vision</h3>
+					<p>change the objective and vision page content </p>
+				</div>
+				<div class="icon">
+					<i class="fas fa-ellipsis-h"></i>
+				</div>
+				<a href="<?php echo Yii::$app->urlManager->createUrl("/schools/school-obj");?>" class="small-box-footer">
+					<i class="fas fa-arrow-circle-right"></i>
+				</a>
+			</div>
 		</div>
-		<div class="col-lg-3 col-sm-2 col-10">
-			<a href="<?php echo Yii::$app->urlManager->createUrl("/schools/school-committee");?>" class="btn btn-outline-info"><?= Html::img('@web/img/indexschool.png',['alt' => 'Nothing to show', 'class' => 'img-fluid']);?><h2 style="font-size: 150%;">School's Committee</h2></a> 
-		</div>
-		<div class="col-lg-3 col-sm-2 col-10">
-			<a href="<?php echo Yii::$app->urlManager->createUrl("/schools/school-office");?>" class="btn btn-outline-info"><?= Html::img('@web/img/indexschool.png',['alt' => 'Nothing to show', 'class' => 'img-fluid']);?><h2 style="font-size: 150%;">School's Office Bearers</h2></a> 
+		<div class="col-lg-5 col-md-6 col-sm-6 col-xs-4 col-10">
+			<div class="small-box bg-light">
+				<div class="inner">
+					<h3>Governanace Body</h3>
+					<p>add, remove or update a member </p>
+				</div>
+				<div class="icon">
+					<i class="fas fa-edit"></i>
+				</div>
+				<a href="<?php echo Yii::$app->urlManager->createUrl("/schools/school-gov-council");?>" class="small-box-footer">
+					<i class="fas fa-arrow-circle-right"></i>
+				</a>
+			</div>
 		</div>
 	</div>
-</div>	
+	<div class="row align-items-center">
+		<div class="col-lg-5 col-md-6 col-sm-6 col-xs-4 col-10">
+			<div class="small-box bg-purple">
+				<div class="inner">
+					<h3>Committee</h3>
+					<p>add, remove or update a member </p>
+				</div>
+				<div class="icon">
+					<i class="fab fa-mendeley"></i>
+				</div>
+				<a href="<?php echo Yii::$app->urlManager->createUrl("/schools/school-committee");?>" class="small-box-footer">
+					<i class="fas fa-arrow-circle-right"></i>
+				</a>
+			</div>
+		</div>
+		<div class="col-lg-5 col-md-6 col-sm-6 col-xs-4 col-10">
+			<div class="small-box bg-light">
+				<div class="inner">
+					<h3>Office Bearers</h3>
+					<p>add, remove or update a member</p>
+				</div>
+				<div class="icon">
+					<i class="fab fa-wpforms"></i>
+				</div>
+				<a href="<?php echo Yii::$app->urlManager->createUrl("/schools/school-office");?>" class="small-box-footer">
+					<i class="fas fa-arrow-circle-right"></i>
+				</a>
+			</div>
+		</div>
+	</div>
+
+</div>
+
+
+
+
 
 <?php 
 	$session->close();
